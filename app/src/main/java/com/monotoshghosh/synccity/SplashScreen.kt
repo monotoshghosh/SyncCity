60e100e97847688855b62adbfe46cceeca98fae0
monotoshghosh <monotoshghosh49@gmail.com>
2024-08-26 22:32:10 +0530

    Splash Animation Added

--- app/src/main/java/com/monotoshghosh/synccity/SplashScreen.kt
+++ app/src/main/java/com/monotoshghosh/synccity/SplashScreen.kt
@@ -1,12 +1,19 @@
 package com.monotoshghosh.synccity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.bumptech.glide.Glide
 import com.monotoshghosh.synccity.databinding.ActivityContactOtherDeptBinding
 import com.monotoshghosh.synccity.databinding.ActivitySplashScreenBinding
 
@@ -23,12 +30,34 @@ class SplashScreen : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        hideNavigationBar()
+
+//        val backBtnGif= binding.animationVideo
+//        Glide.with(this).asGif().load(R.drawable.aniv1).into(backBtnGif)
+
+        // Load and apply the animation to the TextView
+        val teamRbtTextView = binding.root.findViewById<TextView>(R.id.textViewSplashScr)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in_scale)
+        teamRbtTextView.startAnimation(animation)
+
 
         // SPLASH SCREEN
         Handler().postDelayed({
             intent = Intent(this,MainActivity::class.java   )
             startActivity(intent)
             finish()
-        },1500)
+        },2000)
+    }
+
+    private fun hideNavigationBar() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
