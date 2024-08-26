6872c5357bca9043b2755c10ba0ba4b72c7a2223
monotoshghosh <monotoshghosh49@gmail.com>
2024-08-26 22:04:51 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/monotoshghosh/synccity/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.monotoshghosh.synccity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.monotoshghosh.synccity.databinding.ActivityContactOtherDeptBinding
+import com.monotoshghosh.synccity.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+//        enableEdgeToEdge()
+        setContentView(binding.root)
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        // SPLASH SCREEN
+        Handler().postDelayed({
+            intent = Intent(this,MainActivity::class.java   )
+            startActivity(intent)
+            finish()
+        },1500)
+    }
+}
